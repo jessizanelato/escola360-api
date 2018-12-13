f2cfcf47b95004ac17b3eaf368f15d5e2ef833b9
Jessica Zanelato <jessizanelato@gmail.com>
2018-12-12 23:30:19 -0200

    First commit

---
+++ api/controllers/students.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var util = require('util');
+
+module.exports = {
+  getStudents: getStudents,
+  addStudent: addStudent,
+  getStudent: getStudent,
+  updateStudent: updateStudent
+};
+
+/*
+  Functions in a127 controllers used for operations should take two parameters:
+
+  Param 1: a handle to the request object
+  Param 2: a handle to the response object
+ */
+function getStudents(req, res) {
+// todo
+}
+
+function addStudent(req, res) {
+// todo
+}
+
+function getStudent(req, res) {
+// todo
+}
+
+function updateStudent(req, res) {
+// todo
+}
